9e29eafda7d7cd7e4eaefa715248e661241b4a97
~narmyl-hoctyl <sashasylona@gmail.com>
2021-01-22 23:45:50 -0500

    Add edit command

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import nonvolatile
 import peek
 import sys
 import subprocess
+import os
+from xdg import xdg_data_home
 
 def main():
     if len(sys.argv) < 2:
@@ -11,6 +13,9 @@ def main():
         dests = nonvolatile.get_all_dests()
         peek.peek_pdf(dests)
         subprocess.run(["zathura", "peek.pdf"])
+    elif cmd.lower() == "edit":
+        surf_dir = xdg_data_home().joinpath("surfeit/")
+        subprocess.run(["nvim", "-p", "inbox", "next-actions", "projects", "calendar", "waiting-for", "someday-maybe", "reference", "tickler"], cwd=surf_dir)
     else:
         raise "Unknown command. Exiting"
 
